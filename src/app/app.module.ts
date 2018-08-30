d3d06f5e1137146288840fd50012becd4312d505
Rani Abu Sultan <raniabusultan@gmail.com>
2018-08-31 02:18:33 +0300

    Using UIKIT for the UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BookService } from './book.service'
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
 import { MatCardModule } from '@angular/material/card'
+import { MatGridListModule } from '@angular/material/grid-list';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 
@@ -17,6 +18,7 @@ import { BooksComponent } from './books/books.component';
     FormsModule,
     HttpClientModule,
     MatCardModule,
+    MatGridListModule
   ],
   providers: [BookService],
   bootstrap: [AppComponent]
